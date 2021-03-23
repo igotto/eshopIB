434cc9a59b2323ed21ca561c0b6cd785be0e044e
Igo B <igobusarov@yahoo.com>
2021-03-23 23:07:29 +0200

    Front End and React Routing

---
+++ client/src/screens/HomeScreen.js
@@ -0,0 +1,22 @@
+import products from '../products';
+import {Row, Col } from 'react-bootstrap';
+import Products from '../components/Product';
+
+const HomeScreen = () => {
+    return (
+        <>
+
+            <h1>Latest Products</h1>
+            <Row>
+                {products.map((product) => (
+                    <Col key={product._id} sm={12} md={6} lg={3}>
+                        <Products product={product} />
+                    </Col>
+                ))}
+            </Row>
+            
+        </>
+    );
+}
+
+export default HomeScreen
