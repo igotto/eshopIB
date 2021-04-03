1e2160b7f91d90aae999d201a3f2ef2ed37445cb
Igo B <igobusarov@yahoo.com>
2021-04-03 20:43:43 +0300

    Add Cart Functunality

--- client/src/App.js
+++ client/src/App.js
@@ -4,20 +4,22 @@ import Footer from "./components/Footer";
 import { Container } from "react-bootstrap";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
+import CartScreen from './screens/CartScreen';
 
 function App() {
   return (
     <div className="classic">
-    <Router>
-      <Header />
-      <main className="py-3">
-        <Container>
-          <Route path="/" component={HomeScreen} exact />
-          <Route path="/product/:id" component={ProductScreen} exact />
-        </Container>
-      </main>
-      <Footer />
-    </Router>
+      <Router>
+        <Header />
+        <main className="py-3">
+          <Container>
+            <Route path="/" component={HomeScreen} exact />
+            <Route path="/product/:id" component={ProductScreen} exact />
+            <Route path="/cart/:id?" component={CartScreen} exact />
+          </Container>
+        </main>
+        <Footer />
+      </Router>
     </div>
   );
 }
--- client/src/screens/ProductScreen.js
+++ client/src/screens/ProductScreen.js
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import {
@@ -9,13 +9,17 @@ import {
   Card,
   Button,
   ListGroupItem,
+  Form,
 } from "react-bootstrap";
 import Rating from "../components/Rating";
 import Loader from "../components/Loader";
 import Message from "../components/Message";
 import { listProductDetails } from "../actions/productActions";
 
-const ProductScreen = ({ match }) => {
+const ProductScreen = ({ history, match }) => {
+
+  const [qty, setQty] = useState(1);
+
   const dispatch = useDispatch();
 
   const productDetails = useSelector((state) => state.productDetails);
@@ -26,6 +30,10 @@ const ProductScreen = ({ match }) => {
   useEffect(() => {
     dispatch(listProductDetails(match.params.id));
   }, [dispatch, match]);
+
+  const addToCartHandler = () => {
+    history.push(`/cart/${match.params.id}?qty=${qty}`)
+  }
 
   return (
     <>
@@ -77,9 +85,26 @@ const ProductScreen = ({ match }) => {
                     </Col>
                   </Row>
                 </ListGroupItem>
+
+                {product.countInStock > 0 && (
+                  <ListGroup.Item>
+                    <Row>
+                      <Col>Qty</Col>
+                      <Col>
+                        <Form.Control as='select' value={qty}
+                          onChange={(e) => setQty(e.target.value)}>
+                          {[...Array(product.countInStock).keys()].map(x =>
+                            (<option key={x + 1} value={x + 1}>{x + 1}</option>)
+                          )}
+                        </Form.Control>
+                      </Col>
+                    </Row>
+                  </ListGroup.Item>
+                )}
+
                 <ListGroupItem>
                   <Button
-                    className="btn-block"
+                    onClick={addToCartHandler}
                     type="button"
                     disabled={product.countInStock === 0}
                   >
