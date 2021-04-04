53ba8a1d5396403d2b7777ab3289d964174bfe31
Igo B <igobusarov@yahoo.com>
2021-04-04 23:28:59 +0300

    Add Login functionality frontend

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import { Container } from "react-bootstrap";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
 import CartScreen from './screens/CartScreen';
+import LoginScreen from './screens/LoginScreen';
 
 function App() {
   return (
@@ -13,9 +14,10 @@ function App() {
         <Header />
         <main className="py-3">
           <Container>
-            <Route path="/" component={HomeScreen} exact />
+            <Route path="/login" component={LoginScreen} exact />
             <Route path="/product/:id" component={ProductScreen} exact />
             <Route path="/cart/:id?" component={CartScreen} exact />
+            <Route path="/" component={HomeScreen} exact />
           </Container>
         </main>
         <Footer />
