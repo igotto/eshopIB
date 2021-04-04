b0adcd20238eea23478033950906ed07be7f309a
Igo B <igobusarov@yahoo.com>
2021-04-04 19:39:05 +0300

    backend auth

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,11 @@
+import express from 'express';
+const router = express.Router();
+import { authUsers, registerUser, getUserProfile } from '../controllers/userController.js';
+import { protect } from '../middleware/authMiddlewear.js'
+
+router.route('/').post(registerUser)
+router.post('/login', authUsers)
+router.route('/profile').get(protect, getUserProfile)
+
+
+export default router
--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import colors from 'colors';
 import connectDB from './config/db.js';
 // import products from './data/products.js';
 import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 import { notFound, errorHandler } from './middleware/errorMiddlewear.js'
 
 dotenv.config();
@@ -17,12 +18,15 @@ const app = express();
 //     next()
 // })
 
+app.use(express.json())
+
 
 app.get('/', (req, res) => {
     res.send("Running...")
 });
 
 app.use('/api/products', productRoutes)
+app.use('/api/users', userRoutes)
 
 app.use(notFound)
 
